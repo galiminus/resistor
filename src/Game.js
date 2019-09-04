e27442aa69b1efd9983f967f105aeef8ac93a0d9
Victor Goya <goya.victor@gmail.com>
2019-09-04 19:08:52 +0200

    last fixes

--- src/Game.js
+++ src/Game.js
@@ -4,16 +4,41 @@ import { useWindowSize } from 'react-use';
 import Resistor from './Resistor';
 
 const MAX_WIDTH = 600;
-const MAX_RESISTOR_WIDTH = 400;
+const MAX_RESISTOR_WIDTH = 600;
 const RESISTOR_PADDING = 24;
 
+const multiplerValues = [
+  1,
+  10,
+  100,
+  1000,
+  10000,
+  100000,
+  10000000,
+  10000000,
+  0.1,
+  0.01,
+]
+
 function pickResistorValue() {
-  return (220);
+  return (Math.floor(Math.random() * 990) + 10);
+}
+
+function pickResistorMultiplier() {
+  return (multiplerValues[Math.floor(Math.random() * multiplerValues.length)])
+}
+
+function guessAndValueEqual(guess, value, mulitplier) {
+  console.log(guess, value * mulitplier)
+  return (parseInt(guess) === value * mulitplier);
 }
 
 function Game({ classes }) {
   const [value, setValue] = useState(pickResistorValue());
+  const [mulitplier, setMultiplier] = useState(pickResistorMultiplier());
   const [guess, setGuess] = useState("");
+  const [message, setMessage] = useState("Guess the resistor value.");
+  const [success, setSuccess] = useState(0);
 
   let { width: resistorWidth } = useWindowSize();
   if (resistorWidth > MAX_RESISTOR_WIDTH) {
@@ -25,12 +50,28 @@ function Game({ classes }) {
     <div className={classes.root}>
       <h1 className={classes.title}>The <u>Resistor</u> Game</h1>
       <div className={classes.resistorContainer}>
-        <Resistor width={resistorWidth} value={value} stripeWidth={resistorWidth / 16} stripeGap={resistorWidth / 16} />
+        <Resistor width={resistorWidth} value={value} multiplier={mulitplier} stripeWidth={resistorWidth / 20} stripeGap={resistorWidth / 20} />
       </div>
+      <p className={classes.message}>{message}</p>
       <form
         className={classes.inputContainer}
         onSubmit={(e) => {
           e.preventDefault();
+
+          if (guessAndValueEqual(guess, value, mulitplier)) {
+            setSuccess(success + 1);
+
+            if (success === 0) {
+              setMessage(`One resistor value found.`);
+            } else {
+              setMessage(`${success + 1} resistor values found.`);
+            }
+            setValue(pickResistorValue());
+            setMultiplier(pickResistorMultiplier());
+          } else {
+            setMessage("Wrong answer.");
+          }
+          setGuess("");
         }}
       >
         <input
@@ -38,7 +79,7 @@ function Game({ classes }) {
           className={classes.input}
           value={guess}
           onChange={(e) => {
-            if (e.target.value.match(/^[0-9.]*$/) && e.target.value.length < 8) {
+            if (e.target.value.match(/^[0-9.]*$/) && e.target.value.length < 11) {
               setGuess(e.target.value);
             }
           }}
@@ -66,6 +107,10 @@ const styles =  (theme) => ({
     fontSize: 40,
     textAlign: 'center',
   },
+  message: {
+    fontFamily: theme.fontFamily,
+    fontSize: 20,
+  },
   inputContainer: {
     display: 'flex',
     alignItems: 'center',
@@ -79,7 +124,7 @@ const styles =  (theme) => ({
   },
   input: {
     width: '100%',
-    maxWidth: 126,
+    maxWidth: 180,
     padding: 12,
     borderRadius: 4,
     border: '2px solid #333',
--- src/Resistor.js
+++ src/Resistor.js
@@ -13,21 +13,49 @@ const digitMapping = {
   "9": "white",
 }
 
-function valueToColors(value) {
+const multiplierMapping = {
+  1:        "black",
+  10:       "brown",
+  100:      "red",
+  1000:     "orange",
+  10000:    "yellow",
+  100000:   "green",
+  1000000:  "blue",
+  10000000: "violet",
+  0.1:      "gold",
+  0.01:     "silver",
+}
+
+const colorsConfig = {
+  black: { background: "#333333", foreground: 'white' },
+  brown: { background: "#836953", foreground: 'white' },
+  red: { background: "#ff6961", foreground: 'white' },
+  orange: { background: "#ff7514", foreground: 'black' },
+  yellow: { background: "#fdfd96", foreground: 'black' },
+  green: { background: "#77dd77", foreground: 'black' },
+  blue: { background: "#3065ac", foreground: 'white' },
+  violet: { background: "#cb99c9", foreground: 'black' },
+  gray: { background: "#b6afac", foreground: 'black' },
+  white: { background: "#f4f1d6", foreground: 'black' },
+  gold: { background: "#ffc300", foreground: 'black' },
+  silver: { background: "#c2bdb0", foreground: 'black' },
+}
+
+function resistorValueToColors(value, multipler) {
   let colors = [];
 
-  colors.push(digitMapping[new String(value)[0]]);
-  colors.push(digitMapping[new String(value)[1]]);
-  colors.push(digitMapping[new String(value)[2]]);
-
+  [...value.toString()].forEach((digit) => {
+    colors.push(digitMapping[digit]);
+  });
+  colors.push(multiplierMapping[multipler])
   return (colors);
 }
 
-function Resistor({ width, value, strokeWidth = 3, radius = 50, outAngle = 0.2, stripeWidth = 25, stripeGap = 25 }) {
+function Resistor({ width, value, multiplier, pinWidth = 50, strokeWidth = 3, radius = 60, outAngle = 0.2, stripeWidth = 25, stripeGap = 25 }) {
   const canvasRef = useRef(null)
+  const bodyWidth = width - pinWidth * 2;
 
-  const [firstColor, ...otherColors] = valueToColors(value);
-
+  const [firstColor, ...otherColors] = resistorValueToColors(value, multiplier);
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
@@ -38,29 +66,49 @@ function Resistor({ width, value, strokeWidth = 3, radius = 50, outAngle = 0.2, 
     ctx.beginPath();
     ctx.strokeStyle = '#333';
 
-    ctx.arc(radius + strokeWidth + stripeWidth, radius + strokeWidth, radius, Math.PI * (2.5 - outAngle), Math.PI / 2, false);
-    ctx.arc(radius + strokeWidth, radius + strokeWidth, radius, Math.PI * 0.5, Math.PI, false);
-    ctx.arc(radius + strokeWidth, radius + strokeWidth, radius, Math.PI, Math.PI * 1.5, false);
-    ctx.arc(radius + strokeWidth + stripeWidth, radius + strokeWidth, radius, Math.PI * 1.5, Math.PI * (1.5 + outAngle), false);
+    ctx.arc(pinWidth + radius + strokeWidth + stripeWidth, radius + strokeWidth, radius, Math.PI * (2.5 - outAngle), Math.PI / 2, false);
+    ctx.arc(pinWidth + radius + strokeWidth, radius + strokeWidth, radius, Math.PI * 0.5, Math.PI, false);
+    ctx.arc(pinWidth + radius + strokeWidth, radius + strokeWidth, radius, Math.PI, Math.PI * 1.5, false);
+    ctx.arc(pinWidth + radius + strokeWidth + stripeWidth, radius + strokeWidth, radius, Math.PI * 1.5, Math.PI * (1.5 + outAngle), false);
 
-    ctx.arc(width - (radius + strokeWidth + stripeWidth), radius + strokeWidth, radius, Math.PI * (1.5 - outAngle), Math.PI * 1.5, false);
-    ctx.arc(width - (radius + strokeWidth), radius + strokeWidth, radius, Math.PI * 1.5, Math.PI * 2, false);
-    ctx.arc(width - (radius + strokeWidth), radius + strokeWidth, radius, Math.PI * 2, Math.PI * 0.5, false);
-    ctx.arc(width - (radius + strokeWidth + stripeWidth), radius + strokeWidth, radius, Math.PI * 0.5, Math.PI * (0.5 + outAngle), false);
+    ctx.arc(pinWidth + bodyWidth - (radius + strokeWidth + stripeWidth), radius + strokeWidth, radius, Math.PI * (1.5 - outAngle), Math.PI * 1.5, false);
+    ctx.arc(pinWidth + bodyWidth - (radius + strokeWidth), radius + strokeWidth, radius, Math.PI * 1.5, Math.PI * 2, false);
+    ctx.arc(pinWidth + bodyWidth - (radius + strokeWidth), radius + strokeWidth, radius, Math.PI * 2, Math.PI * 0.5, false);
+    ctx.arc(pinWidth + bodyWidth - (radius + strokeWidth + stripeWidth), radius + strokeWidth, radius, Math.PI * 0.5, Math.PI * (0.5 + outAngle), false);
     ctx.closePath();
     ctx.stroke();
 
+    ctx.beginPath();
+    ctx.arc(pinWidth + strokeWidth, (radius - strokeWidth) * 2, pinWidth, Math.PI , Math.PI * 1.5, false);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(pinWidth + bodyWidth - strokeWidth, (radius - strokeWidth) * 2, pinWidth, 0 , Math.PI * 1.5, true);
+    ctx.stroke();
 
     ctx.lineWidth = 1;
+    ctx.font = "bold 20px 'Major Mono Display', monospace";
 
-    ctx.fillStyle = firstColor;
-    ctx.fillRect(radius + strokeWidth, strokeWidth, stripeWidth, radius * 2);
-    ctx.rect(radius + strokeWidth, strokeWidth, stripeWidth, radius * 2);
+    ctx.fillStyle = colorsConfig[firstColor].background;
+    ctx.fillRect(pinWidth + radius + strokeWidth, strokeWidth, stripeWidth, radius * 2);
+    ctx.rect(pinWidth + radius + strokeWidth, strokeWidth, stripeWidth, radius * 2);
+
+    ctx.fillStyle = colorsConfig[firstColor].foreground;
+    ctx.fillText(firstColor[0], pinWidth + radius + strokeWidth + stripeWidth / 2 - 7,  strokeWidth + radius + 10);
 
     otherColors.forEach((color, index) => {
-      ctx.fillStyle = color;
-      ctx.fillRect(radius * 2 + (stripeGap + stripeWidth) * index + strokeWidth * 2, strokeWidth + Math.sin(outAngle) * radius, stripeWidth, radius * 2 - Math.sin(outAngle) * radius * 2);
-      ctx.rect(radius * 2 + (stripeGap + stripeWidth) * index + strokeWidth * 2, strokeWidth + Math.sin(outAngle) * radius, stripeWidth, radius * 2 - Math.sin(outAngle) * radius * 2);
+      const rectX = pinWidth + radius * 2 + (stripeGap + stripeWidth) * index + strokeWidth * 2;
+      const rectY = strokeWidth + Math.sin(outAngle) * radius;
+      const rectWidth = stripeWidth;
+      const rectHeight = radius * 2 - Math.sin(outAngle) * radius * 2;
+
+      ctx.fillStyle = colorsConfig[color].background;
+      ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+      ctx.rect(rectX, rectY, stripeWidth, rectHeight);
+      ctx.stroke();
+
+      ctx.fillStyle = colorsConfig[color].foreground;
+      ctx.fillText(color[0], rectX + rectWidth / 2 - 7,  rectY + rectHeight / 2 + 10);
     });
 
     ctx.stroke();
